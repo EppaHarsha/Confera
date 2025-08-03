68a7abfbeaa0bbf66c18487cc39299a687b36ce2
EppaHarsha <eppaharsha08@gmail.com>
2025-08-03 15:14:59 +0530

    login route

--- frontend/src/Auth/Login.jsx
+++ frontend/src/Auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { backendUrl } from "../utils/config";
 function Login() {
